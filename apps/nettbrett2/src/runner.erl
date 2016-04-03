923073e448b60268bbc582a1ade1e9194e316ded
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-03 02:14:17 +0200

    initial runner

---
+++ apps/nettbrett2/src/runner.erl
@@ -0,0 +1,25 @@
+-module(runner).
+
+-export([
+    start/0,
+    module_loop/2
+]).
+
+
+start() ->
+    start_loop(bandwidth, application:get_env('nettbrett2', bw_timeout, 5000)),
+    start_loop(pong, application:get_env('nettbrett2', pong_timeout, 10000)).
+
+start_loop(Module, Sleep) ->
+    spawn_link(?MODULE, module_loop, [Module, Sleep]).
+
+module_loop(Module, Sleep) ->
+    JSON = case Module of
+        bandwidth ->
+            bandwidth:get();
+        pong ->
+            pong:get()
+    end,
+    ws_broadcast_handler:broadcast(JSON),
+    timer:sleep(Sleep),
+    module_loop(Module, Sleep).
